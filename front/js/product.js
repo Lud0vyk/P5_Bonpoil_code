9f3e906cda40f4d717655994819940bf2fa7ef9b
Lud0vyk <92742011+Lud0vyk@users.noreply.github.com>
2022-03-29 20:05:07 +0200

    29032022
    2004

--- front/js/product.js
+++ front/js/product.js
@@ -3,19 +3,83 @@
  ***                    DECLARATION VARIABLES                   ***
  ******************************************************************/
 
- const kanapImage = document.getElementsByClassName('item__img');
+
+ const kanapImage = document.getElementsByClassName('.item__img');
  const kanapTitre = document.getElementById('title');
  const kanapPrice = document.getElementById('price');
  const kanapDescription = document.getElementById('description');
+ const kanapColors = document.getElementById('colors');
 
  //récupération de l'id du produit
  let queryString_url_id = window.location.search;
- console.log(queryString_url_id);
  let urlSearchParams = new URLSearchParams(queryString_url_id);
- console.log(urlSearchParams);
-
  let id = urlSearchParams.get("id");
  console.log(id);
+ //variable de récupération du produit
+ let productChoice;
+
+// affichage des l'éléments dans product.html
+function showProductById (product) {
+     
+    kanapImage.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}" >`;
+    kanapTitre.innerText = `(${product.name})`;
+    kanapPrice.innerText = `(${product.price})`;
+    kanapDescription.innerText = `(${product.description})`;
+    for(let color of product.colors) {
+        kanapColors.innerHTML +=`<option value="${color}">${color}</option>`;
+    }
+}
+
+ //fonction asynchrone pour récupérer les produits
+ async function mainProduct (){
+
+    const products = await getProduct();
+    let productById = products.find( (element)=> element._id === id);
+    showProductById(productById);
+    console.log(productById)
+    return productChoice = productById;
+ }
+
+  //fonction pour récupérer les produits
+  function getProduct(){
+    return fetch('http://localhost:3000/api/products/')
+    .then( function(reponse) { return reponse.json()})
+    .then( function(products) { return products})
+    .catch( function (error) {alert ('error product')} )
+ }
+
+
+ //sélection des données du panier
+ const quantityKanap = document.querySelector("#quantity");
+ const colorKanap = document.querySelector("#colors");
+
+ //données à envoyer
+ const addToCart = document.querySelector("#addToCart");
+
+
+ //envoi du panier
+ addToCart.addEventListener("click", (event) => {
+     event.preventDefault();
+
+ //récupération de la couleur
+ const selectedColor = colorKanap.value;
+ //récupération de la quantité
+ const selectedQuantity = quantityKanap.value;
+
+  //récupartion du panier
+  let cart = {
+    productId : productChoice._id,
+    productColor : selectedColor,
+    productQuantity : selectedQuantity,
+    productPrice : productChoice.price,
+    totalPrice : selectedQuantity * productChoice.price
+    //voir si besoin d'autre chose
+    }
+
+    console.log(cart);
+ });
+
+
 
 /******************************************************************
  ***                          FONCTIONS                         ***
@@ -30,35 +94,6 @@
  requête json contenant un objet de contact et un tableu produit et order id */
  
 
- 
- // affichage des l'éléments dans product.html
- function showProductById (product) {
-     
-    kanapImage.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}" >`;
-    kanapTitre.innerText = `(${product.name})`;
-    kanapPrice.innerText = `(${product.price})`;
-    kanapDescription.innerText = `(${product.description})`;
- }
-
- //fonction asynchrone pour récupérer les produits
- async function mainProduct (){
-    const products = await getProduct();
-
-    let productById = products.find( (element)=> element._id === id);
-    console.log(productById);
-
-    showProductById(productById);
- }
-
-  //fonction pour récupérer les produits
-  function getProduct(){
-    return fetch('http://localhost:3000/api/products/')
-    .then( function(reponse) { return reponse.json()})
-    .then( function(products) { return products})
-    .catch( function (error) {alert ('error product')} )
- }
-
-
  //fonction asynchrone pour le choix des couleurs
  /*async function productChoice (){
     const products = await getProduct();
@@ -66,14 +101,11 @@
         showProduct(product);
     }
  }*/
+
 /******************************************************************
  ***                       CODE PRINCIPAL                       ***
  ******************************************************************/
-  
- //fonction principale de la page product
+
+
+ //fonction principale de la page product v3
  mainProduct();
-
-
-
- 
- 
